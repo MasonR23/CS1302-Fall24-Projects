acda70aef31a731fc1fe85f0f3d872b33041fb3a
Mason Gauge Richardson <mricha23@my.westga.edu>
2024-09-25 18:21:12 -0400

    Can Now Add Items
    This commit connects the codebehind and the main food class togeather so that not only can food objects be created, also allows for the user to set the begging quantity of the item. also allows for the user to see what items are in the pantry by using a listView to display in text their items currently in their pantry.

--- Code/Food Pantry/src/edu/westga/cs1302/FoodPantry/view/MainWindow.java
+++ Code/Food Pantry/src/edu/westga/cs1302/FoodPantry/view/MainWindow.java
@@ -1,7 +1,13 @@
 package edu.westga.cs1302.FoodPantry.view;
 
+import java.util.ArrayList;
+
+import edu.westga.cs1302.FoodPantry.model.Food;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -13,16 +19,51 @@ import javafx.scene.control.TextField;
  */
 public class MainWindow {
 
+	 private String[] types = {"Vegetable", "Meat", "Bread", "Fruit", "Dessert", "Ingredient"};
+	 private Integer[] quantity = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 50, 100};
+	 private ArrayList<Food> pantry = new ArrayList<Food>(); 
+	 private ArrayList<String> visualRepresentation = new ArrayList<String>();
     @FXML
-    private ListView<?> foodListView;
+    private ListView<String> foodListView;
 
     @FXML
     private TextField foodName;
 
     @FXML
-    private TextField foodType;
+    private ChoiceBox<String> foodType;
+    
+    @FXML
+    private ChoiceBox<Integer> foodQuantity;
 
     @FXML
     private Button submitFoodItem;
+    
+    /**this handles when the submitFoodItem button is pressed. 
+     * 
+     * @author Mason Richardson
+     * @version Fall 2024
+     * 
+     * @param event this is the even that is fired by the button "submitFoodItem".
+     */
+    public void addFoodToPantry(ActionEvent event) {
+    	try {
+    		Food newFood = new Food(this.foodName.getText(), this.foodType.getValue());
+    		newFood.changeFoodQuantity(this.foodQuantity.getValue());
+    		this.pantry.add(newFood);
+        	this.visualRepresentation.add(newFood.getFoodName() + " " + newFood.getfoodType() + " " + newFood.getFoodQuantity());
+        	this.foodListView.getItems().clear();
+        	this.foodListView.getItems().addAll(this.visualRepresentation);
+    	} catch (Exception error) {
+    		Alert alert = new Alert(Alert.AlertType.ERROR);
+    		alert.setContentText(error.getMessage());
+    		alert.showAndWait();
+    	}
+    }
+    
+    @FXML
+	void initialize() {
+		this.foodType.getItems().addAll(this.types);
+		this.foodQuantity.getItems().addAll(this.quantity);
+	}
 
 }
