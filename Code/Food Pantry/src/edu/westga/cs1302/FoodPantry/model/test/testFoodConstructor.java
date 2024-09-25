7af254cb9d66ef1b6aa9f5b108f9a81841b70eff
Mason Gauge Richardson <mricha23@my.westga.edu>
2024-09-25 02:45:38 -0400

    Testing Nesscary Methods
    This commit adds the test that were needed to make sure that the consructor works as intended. No other methods needed testing because of how simple their logic is.

--- Code/Food Pantry/src/edu/westga/cs1302/FoodPantry/model/test/testFoodConstructor.java
+++ Code/Food Pantry/src/edu/westga/cs1302/FoodPantry/model/test/testFoodConstructor.java
@@ -4,11 +4,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.FoodPantry.model.Food;
+
 class testFoodConstructor {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testWhenNameIsNull() {
+		 assertThrows(IllegalArgumentException.class,
+		            ()->{
+		            Food Mango = new Food (null , "fruit");
+		            });
+	}
+	
+	@Test
+	public void testWhenTypeIsNull() {
+		 assertThrows(IllegalArgumentException.class,
+		            ()->{
+		            Food Mango = new Food ("mango" , null);
+		            });
+	}
+	
+	@Test
+	public void testWhenConstructorShouldCreateObject() {
+		 Food Mango = new Food ("Mango" , "fruit");
+		 
+		 assertTrue(Mango.getFoodName().equals("Mango")); 
+		 assertTrue(Mango.getfoodType().equals("fruit"));
+		 assertTrue(Mango.getFoodQuantity() == 0);
 	}
 
 }
